adfad9acbef100949c7b9506b264a3c528f8f831
jitam <jitender.sridhar@gmail.com>
2025-05-15 19:50:03 +0530

    change in pdf

--- src/pages/index.js
+++ src/pages/index.js
@@ -35,26 +35,24 @@ export default function Home() {
                 className="!text-6xl !text-left xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-4xl sm:!text-3xl"
               />
               <p className="my-4 text-base font-medium md:text-sm sm:text-xs">
-                As a skilled frontend developer, I am dedicated to turning
-                ideas into innovative web/mobile applications. Explore my latest
-                projects and articles, showcasing my expertise in React.js, Next.js, React-Native and
-                web development.
+                As a skilled frontend developer, I am dedicated to turning ideas
+                into innovative web/mobile applications. Explore my latest
+                projects and articles, showcasing my expertise in React.js,
+                Next.js, React-Native and web development.
               </p>
               <div className="flex items-center self-start mt-2 lg:self-center">
                 <Link
                   href="/resume.pdf"
                   target="_blank"
                   download={true}
-                  className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-light hover:border-dark mx-4 dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:p-2 md:px-4 md:text-base"
-                >
+                  className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-light hover:border-dark mx-4 dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:p-2 md:px-4 md:text-base">
                   Resume
                   <LinkArrow className={"w-6 ml-1"} />
                 </Link>
                 <Link
                   href="mailto:jitender.sridhar@gmail.com"
                   className="text-dark dark:text-light underline font-medium text-lg capitalize md:text-base"
-                  target="_blank"
-                >
+                  target="_blank">
                   Contact
                 </Link>
               </div>
